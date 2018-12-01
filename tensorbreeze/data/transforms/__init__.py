6be0a0dea789fedacc7e4523bd28cb19120169b4
mingruimingrui <mingruimingrui@hotmail.com>
2018-12-01 15:20:28 +0800

    added basic image transforms

---
+++ tensorbreeze/data/transforms/__init__.py
@@ -0,0 +1,15 @@
+from .transforms import (
+    Compose,
+    ImageResize,
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+    ImageNormalization
+)
+
+__all__ = [
+    'Compose',
+    'ImageResize',
+    'RandomHorizontalFlip',
+    'RandomVerticalFlip',
+    'ImageNormalization'
+]
