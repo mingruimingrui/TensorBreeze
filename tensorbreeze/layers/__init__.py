654acb548b826d697cf3adc59cf321c4b96307ed
mingruimingrui <mingruimingrui@hotmail.com>
2018-12-05 18:01:53 +0800

    added focal loss

--- tensorbreeze/layers/__init__.py
+++ tensorbreeze/layers/__init__.py
@@ -1,10 +1,12 @@
 from ._pad import pad2d
 from ._change_format import to_nchw, to_nhwc
 from ._anchor import compute_anchors
+from ._focal_loss import sigmoid_focal_loss
 
 __all__ = [
     'pad2d',
     'to_nchw',
     'to_nhwc',
-    'compute_anchors'
+    'compute_anchors',
+    'sigmoid_focal_loss'
 ]
