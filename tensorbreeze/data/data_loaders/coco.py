fixed bug with normalization function

tensorbreeze/data/data_loaders/coco.py
@@ -99,7 +99,11 @@ def enqueue_thread_main(
     for batch in data_loader:
         feed_dict = dict(zip(annotation_placeholders, batch['annotations']))
         feed_dict[image_placeholder] = batch['image']
-        sess.run([enqueue_op, enqueue_shape_op], feed_dict=feed_dict)
+        try:
+            sess.run([enqueue_op, enqueue_shape_op], feed_dict=feed_dict)
+        except tf.errors.CancelledError:
+            # Quit upon closing of session
+            break
 
 
 def add_coco_loader_ops(

tensorbreeze/data/transforms/transforms.py
@@ -16,6 +16,10 @@ logger = logging.getLogger(__name__)
 VGG_MEAN = [123.675, 116.28, 103.53]
 VGG_STD = [58.395, 57.12, 57.375]
 
+# format VGG weights to more easily be applied to image arrays
+VGG_MEAN = np.array(VGG_MEAN)[np.newaxis, np.newaxis, :]
+VGG_STD = np.array(VGG_STD)[np.newaxis, np.newaxis, :]
+
 
 def check_image_is_numpy(image):
     assert isinstance(image, np.ndarray)
@@ -191,7 +195,5 @@ class ImageNormalization(object):
 
     def __call__(self, item):
         check_image_is_numpy(item['image'])
-        item['image'][..., 0] = (item['image'][..., 0] - VGG_MEAN[0]) / VGG_STD[0]
-        item['image'][..., 1] = (item['image'][..., 1] - VGG_MEAN[1]) / VGG_STD[1]
-        item['image'][..., 2] = (item['image'][..., 2] - VGG_MEAN[2]) / VGG_STD[2]
+        item['image'] = (item['image'] - VGG_MEAN) / VGG_STD
         return item