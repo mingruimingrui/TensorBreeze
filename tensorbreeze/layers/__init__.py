0727c2fd539c3cf8d4f81d0bc5525f6d32e3c957
mingruimingrui <mingruimingrui@hotmail.com>
2019-02-12 23:05:01 +0800

    added meter for loss tracking

--- tensorbreeze/layers/__init__.py
+++ tensorbreeze/layers/__init__.py
@@ -2,11 +2,14 @@ from ._pad import pad2d
 from ._change_format import to_nchw, to_nhwc
 from ._anchor import compute_anchors
 from ._focal_loss import focal_loss
+from ._meter import add_meter_ops, add_meter_dict_ops
 
 __all__ = [
     'pad2d',
     'to_nchw',
     'to_nhwc',
     'compute_anchors',
-    'focal_loss'
+    'focal_loss',
+    'add_meter_ops',
+    'add_meter_dict_ops'
 ]
---
+++ tensorbreeze/layers/_meter.py
@@ -0,0 +1,56 @@
+from collections import Mapping
+import tensorflow as tf
+
+
+def add_meter_ops(
+    loss,
+    decay=0.01,
+    name=None,
+    init_value=0
+):
+    """ Meter for recording loss """
+    with tf.variable_scope('meter/'):
+        x = tf.Variable(
+            initial_value=init_value,
+            trainable=False,
+            validate_shape=False,
+            dtype=tf.float32
+        )
+        sess = tf.get_default_session()
+        sess.run(x.initializer)
+
+        y = x * (1 - decay) + decay * loss
+        y = tf.assign(x, y, validate_shape=False, name=name)
+
+    return y, y.op
+
+
+def add_meter_dict_ops(
+    loss_dict,
+    decay=0.01,
+    names=None,
+    init_values=0
+):
+    """ Meter for recording loss dict """
+    meter_dict = {}
+    meter_dict_ops = {}
+
+    for key, loss in loss_dict.items():
+        if isinstance(init_values, Mapping):
+            init_value = init_values[key]
+        else:
+            init_value = init_values
+
+        if isinstance(names, Mapping):
+            name = names[key]
+        else:
+            name = None
+
+        meter_dict[key], meter_dict_ops[key] = add_meter_ops(
+            loss,
+            decay=decay,
+            name=name,
+            init_value=init_value
+        )
+
+    return meter_dict, meter_dict_ops
