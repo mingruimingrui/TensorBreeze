b08ee6c3bb4ad740d4c9c8e4faccc61e4781402a
mingruimingrui <mingruimingrui@hotmail.com>
2019-03-31 21:28:53 +0800

    added triplet data_loader ops

--- tensorbreeze/data/data_loaders/triplet.py
+++ tensorbreeze/data/data_loaders/triplet.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 from __future__ import division
 
-from six import string_types, integer_types
+import sys
+import threading
+from six import integer_types
 
-from torch.utils.data import DataLoader
-
+import tensorflow as tf
 from torchvision import transforms
+from torch.utils.data import DataLoader
 
 from ..datasets import TripletDataset
 from ..transforms import VGG_MEAN, VGG_STD
@@ -63,3 +65,104 @@ def make_triplet_data_loader(
     )
 
     return data_loader
+
+
+def enqueue_thread_main(
+    sess,
+    enqueue_op,
+    data_loader,
+    anchor_placeholder,
+    positive_placeholder,
+    negative_placeholder
+):
+    data_iter = iter(data_loader)
+    while True:
+        # Get next batch
+        try:
+            (a, p, n), _ = next(data_iter)
+        except StopIteration:
+            data_iter = iter(data_loader)
+            (a, p, n) = next(data_iter)
+
+        # Create feed dict
+        feed_dict = {
+            anchor_placeholder: a,
+            positive_placeholder: p,
+            negative_placeholder: n
+        }
+
+        # Enqueue
+        try:
+            sess.run(enqueue_op, feed_dict=feed_dict)
+        except tf.errors.CancelledError:
+            # Quit upon closing of session
+            break
+
+
+def add_triplet_loader_ops(
+    sess,
+    root_image_dir,
+    batch_size=1,
+    num_workers=2,
+    height=224,
+    width=224,
+    random_horizontal_flip=False,
+    random_vertical_flip=False,
+    scope='data_loader'
+):
+    """
+    Triplet data loader implemented with multiprocessing data loader and
+    threaded enqueue dequeue
+    """
+    data_loader = make_triplet_data_loader(
+        root_image_dir=root_image_dir,
+        num_iter=2 ** 31 - 1,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        height=height,
+        width=width,
+        random_horizontal_flip=random_horizontal_flip,
+        random_vertical_flip=random_vertical_flip
+    )
+
+    with tf.name_scope(scope):
+        input_dtype = tf.float32
+        input_shape = (batch_size, 3, height, width)
+
+        # Create placeholder tensors
+        anchor_placeholder = tf.placeholder(
+            dtype=input_dtype, shape=input_shape)
+        positive_placeholder = tf.placeholder(
+            dtype=input_dtype, shape=input_shape)
+        negative_placeholder = tf.placeholder(
+            dtype=input_dtype, shape=input_shape)
+
+        # Create queues
+        batch_queue = tf.FIFOQueue(
+            capacity=2,
+            shapes=(input_shape,) * 3,
+            dtypes=(input_dtype,) * 3
+        )
+
+        # Create enqueue dequeue ops
+        enqueue_op = batch_queue.enqueue((
+            anchor_placeholder,
+            positive_placeholder,
+            negative_placeholder
+        ))
+        anchor_tensor, positive_tensor, negative_tensor = \
+            batch_queue.dequeue()
+
+        # Start enqueue threads
+        t = threading.Thread(target=enqueue_thread_main, kwargs={
+            'sess': sess,
+            'enqueue_op': enqueue_op,
+            'data_loader': data_loader,
+            'anchor_placeholder': anchor_placeholder,
+            'positive_placeholder': positive_placeholder,
+            'negative_placeholder': negative_placeholder
+        })
+        t.setDaemon(True)
+        t.start()
+
+    return anchor_tensor, positive_tensor, negative_tensor
